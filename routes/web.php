ec3dc3f7ed5d630b98453fc3e8f0748940745c8d
Vasileios Bakas <vasileios.bakas92@gmail.com>
2022-12-27 18:17:11 +0200

    29

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 // User Related routes
-Route::get('/', [UserController::class,"showCorrectHomepage"]);
-Route::post('/register', [UserController::class,'register']);
-Route::post('/login', [UserController::class,'login']);
-Route::post('/logout', [UserController::class,'logout']);
+Route::get('/', [UserController::class,"showCorrectHomepage"])->name('login');
+Route::post('/register', [UserController::class,'register'])->middleware('guest');
+Route::post('/login', [UserController::class,'login'])->middleware('guest');
+Route::post('/logout', [UserController::class,'logout'])->middleware('auth');
 
 // Blog related routes
-Route::get('/create-post',[PostController::class,'showCreateForm']);
-Route::post('/create-post',[PostController::class,'storeNewPost']);
+Route::get('/create-post',[PostController::class,'showCreateForm'])->middleware('guest');
+Route::post('/create-post',[PostController::class,'storeNewPost'])->middleware('auth');
 Route::get('/post/{post}',[PostController::class,'viewSinglePost']);
